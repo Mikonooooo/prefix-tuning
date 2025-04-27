5d9153cc8cf297be3c2d1bd4d0ef5a163e93621c
Mac Turner <macturner8@gmail.com>
2025-04-27 00:33:07 -0400

    rewrote dataset

--- code/data_gen.py
+++ code/data_gen.py
@@ -1,7 +1,8 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import sys, os
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 import numpy
 
 def get_dict_from_data(filepath:str, tokenizer: GPT2Tokenizer) -> list[str]:
@@ -18,59 +19,60 @@ def get_dict_from_data(filepath:str, tokenizer: GPT2Tokenizer) -> list[str]:
     return input_output_dict
 
 class e2eDataset(Dataset):
-    def __init__(self, filepath, tokenizer):
+    def __init__(self, filepath, tokenizer, max_length=256):
         self.tokenizer = tokenizer
         self.examples = []
+        self.max_length = max_length
+
         with open(filepath, "r") as f:
             for i, line in enumerate(f):
                 line = line.strip()
-                input_table, output_sent = line.split("||")
-                input_table = ' {} {}'.format(input_table, tokenizer.bos_token)
-                output_sent = ' {} {}'.format(output_sent, tokenizer.eos_token)
-                
-                self.examples.append({"table": input_table, "text": output_sent})
+                table, text = line.split("||")
+                table = ' {} {}'.format(table, tokenizer.bos_token)
+                text = ' {} {}'.format(text, tokenizer.eos_token)   
+                self.examples.append((table, text))
 
     def __len__(self):
         return len(self.examples)
 
     def __getitem__(self, index):
-        example = self.examples[index]
-        table, text = example["table"], example["text"]
-        table_tokens = self.tokenizer(table, return_tensors="pt")
-        text_tokens = self.tokenizer(text, return_tensors="pt")
-
-        table_len = len(table_tokens.input_ids[0])
-
-        input_ids = torch.cat([table_tokens.input_ids.squeeze(0), text_tokens.input_ids.squeeze(0)])
-
-        labels = input_ids.clone()
-        labels[:table_len] = -100
-
-        src_attn = torch.zeros_like(input_ids)
-        src_attn[:table_len] = 1
-        
-        tgt_attn = torch.zeros_like(input_ids)
-        tgt_attn[table_len:] = 1
+        table, text = self.examples[index]
+        tokenized_table = self.tokenizer(table, truncation=True)
+        tokenized_text = self.tokenizer(text, truncation=True)
 
         return {
-            "input_ids": input_ids,
-            "labels": labels,
-            "src_attn": src_attn,
-            "tgt_attn": tgt_attn,
-            "src": table
+            "input_ids": tokenized_table["input_ids"],
+            "attention_mask": tokenized_table["attention_mask"],
+            "labels": tokenized_text["input_ids"]
         }
-
-
-
-
-
-
+    
+def collate_fn(batch, pad_token_id):
+    input_ids = [torch.tensor(item["input_ids"]) for item in batch]
+    attention_mask = [torch.tensor(item["attention_mask"]) for item in batch]
+    labels = [torch.tensor(item["labels"]) for item in batch]
+    
+    input_ids_padded = pad_sequence(input_ids, batch_first=True, padding_value=pad_token_id)
+    attention_mask_padded = pad_sequence(attention_mask, batch_first=True, padding_value=0)
+    labels_padded = pad_sequence(labels, batch_first=True, padding_value=-100)
+    
+    return {
+        "input_ids": input_ids_padded,
+        "attention_mask": attention_mask_padded,
+        "labels": labels_padded
+    }
 
 
 
 if __name__ == "__main__":
     filepath = "data/e2e_data/src1_test.txt"
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
     # get_dict_from_data(filepath, tokenizer)
     dataset = e2eDataset(filepath, tokenizer)
-    
+    print(dataset.__getitem__(0))
+
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=False, collate_fn=lambda batch: collate_fn(batch, tokenizer.pad_token_id))
+    for b in dataloader:
+        print(b)
+        break
+
