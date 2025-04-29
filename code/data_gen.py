d9e1dcee389b96eb301de7f2cb13b7f303e24e2d
Michael Ngo <mn542@cornell.edu>
2025-04-28 20:33:19 -0400

    add randomized data gen if data_gen.py

--- code/data_gen.py
+++ code/data_gen.py
@@ -5,98 +5,37 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 import numpy
+import random
 
 
-def get_dict_from_data(filepath: str, tokenizer: GPT2Tokenizer) -> list[str]:
-    input_output_dict = {}
-    with open(filepath, "r") as f:
-        for i, line in enumerate(f):
-            line = line.strip()
-            input_table, output_sent = line.split("||")
-            input_table = ' {} {}'.format(input_table, tokenizer.bos_token)
-            output_sent = ' {} {}'.format(output_sent, tokenizer.eos_token)
-
-            input_output_dict.setdefault(input_table, [])
-            # append all sentences that map from table data
-            input_output_dict[input_table].append(output_sent)
-    return input_output_dict
-
-
-class e2eDataset(Dataset):
-    def __init__(self, filepath, tokenizer):
-        self.tokenizer = tokenizer
-        self.examples = []
-
-        with open(filepath, "r") as f:
-            for i, line in enumerate(f):
-                line = line.strip()
-                table, text = line.split("||")
-                example = ' {} {} {} {}'.format(
-                    table, tokenizer.bos_token, text, tokenizer.eos_token)
-                # text = ' {} {}'.format(text, tokenizer.eos_token)
-                self.examples.append(example)
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, index):
-        # table, text = self.examples[index]
-        # tokenized_table = self.tokenizer(table, truncation=True)
-        # tokenized_text = self.tokenizer(text, truncation=True)
-        tokenized = self.tokenizer(self.examples[index], truncation=True)
-
-        # mask out table in labels
-        sep_idx = tokenized["input_ids"].index(self.tokenizer.bos_token_id)
-        labels = tokenized["input_ids"].copy()
-        labels[:sep_idx] = [-100]*sep_idx
-
-        return {
-            "input_ids": tokenized["input_ids"],
-            "attention_mask": tokenized["attention_mask"],
-            "labels": labels
-        }
-
-
-def collate_fn(batch, pad_token_id):
-    input_ids = [torch.tensor(item["input_ids"]) for item in batch]
-    attention_mask = [torch.tensor(item["attention_mask"]) for item in batch]
-    labels = [torch.tensor(item["labels"]) for item in batch]
-
-    input_ids_padded = pad_sequence(
-        input_ids, batch_first=True, padding_value=pad_token_id)
-    attention_mask_padded = pad_sequence(
-        attention_mask, batch_first=True, padding_value=0)
-    labels_padded = pad_sequence(labels, batch_first=True, padding_value=-100)
-
-    return {
-        "input_ids": input_ids_padded,
-        "attention_mask": attention_mask_padded,
-        "labels": labels_padded
-    }
-
-
-def make_dataloaders(files, tokenizer, batch_size):
-    datasets = {}
-    for name, filepath in files.items():
-        if filepath is not None:
-            dataset = e2eDataset(filepath, tokenizer)
-            dataloader = DataLoader(dataset, batch_size, shuffle=True,
-                                    collate_fn=lambda batch: collate_fn(batch, tokenizer.pad_token_id))
-            datasets[name] = dataloader
-    return datasets
+def gen_rand_subset(filepath, num_samples, outfilepath):
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+        print(lines[1])
+    subset = random.choices(lines, k=num_samples)
+    with open(outfilepath, 'w') as f:
+        f.writelines(subset)
 
 
 if __name__ == "__main__":
-    filepath = "data/e2e_data/src1_test.txt"
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    # print(tokenizer.bos_token, tokenizer.eos_token, tokenizer.pad_token)
-    tokenizer.pad_token = tokenizer.eos_token
-    # get_dict_from_data(filepath, tokenizer)
-    dataset = e2eDataset(filepath, tokenizer)
-    print(dataset.__getitem__(0))
+    base = "data/e2e_data/"
 
-    dataloader = DataLoader(dataset, batch_size=2, shuffle=False,
-                            collate_fn=lambda batch: collate_fn(batch, tokenizer.pad_token_id))
-    for batch in dataloader:
-        print(batch)
-        break
+    # Generate small train dataset
+    filepath = base + "src1_train.txt"
+    outfilepath = base + "small_train.txt"
+    gen_rand_subset(filepath, 100, outfilepath)
+
+    # Generate medium train dataset
+    filepath = base + "src1_train.txt"
+    outfilepath = base + "medium_train.txt"
+    gen_rand_subset(filepath, 500, outfilepath)
+
+    # Generate medium valid dataset
+    filepath = base + "src1_valid.txt"
+    outfilepath = base + "medium_val.txt"
+    gen_rand_subset(filepath, 500, outfilepath)
+
+    # Generate medium test dataset
+    filepath = base + "src1_test.txt"
+    outfilepath = base + "medium_test.txt"
+    gen_rand_subset(filepath, 500, outfilepath)
--- code/train.py
+++ code/train.py
@@ -1,6 +1,6 @@
 import torch
 import torch.optim as optim
-from data_gen import make_dataloaders
+from data_load import make_dataloaders
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, get_linear_schedule_with_warmup
 from prefix_tuner import PrefixTuning
 from tqdm import tqdm
@@ -12,13 +12,14 @@ import os
 
 DATA_PATH = "data/e2e_data/"
 
+
 def tune(tuner, dataset, num_epochs, batch_size, prefix_len, lr):
     if dataset == 'small':
-        files = {"train": DATA_PATH + "small_data.txt",
-                 "val": None}
+        files = {"train": DATA_PATH + "small_train.txt",
+                 "val": DATA_PATH + "medium_val.txt"}
     elif dataset == 'medium':
-        files = {"train": DATA_PATH + "medium_data.txt",
-                 "val": DATA_PATH + "medium_data_val.txt"}
+        files = {"train": DATA_PATH + "medium_train.txt",
+                 "val": DATA_PATH + "medium_val.txt"}
     elif dataset == 'full':
         files = {"train": DATA_PATH + "src1_train.txt",
                  "val": DATA_PATH + "src1_valid.txt"}
@@ -35,11 +36,13 @@ def tune(tuner, dataset, num_epochs, batch_size, prefix_len, lr):
     dataloaders = make_dataloaders(files, tokenizer, batch_size=batch_size)
     print(dataloaders)
     train_dataloader = dataloaders["train"]
-    val_dataloader = dataloaders["val"] if "val" in dataloaders.keys() else None
+    val_dataloader = dataloaders["val"] if "val" in dataloaders.keys(
+    ) else None
 
     optimizer = optim.AdamW(model.parameters(), lr=lr)
-    train_losses, val_losses = train(model, optimizer, train_dataloader, val_dataloader, num_epochs)
-    
+    train_losses, val_losses = train(
+        model, optimizer, train_dataloader, val_dataloader, num_epochs)
+
     return model, train_losses, val_losses
 
 
@@ -48,7 +51,8 @@ def train(model, optimizer, train_dataloader, val_dataloader=None, epochs=10):
     train_losses = []
     val_losses = []
 
-    scheduler = get_linear_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=100, num_training_steps = epochs*len(train_dataloader))
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer, num_warmup_steps=100, num_training_steps=epochs*len(train_dataloader))
 
     model.train()
     for i in range(epochs):
@@ -69,13 +73,13 @@ def train(model, optimizer, train_dataloader, val_dataloader=None, epochs=10):
         train_losses.append(avg_train_loss)
         print(f"epoch {i+1} loss: {avg_train_loss}")
 
-
-        if val_dataloader is not None and i%2 == 1:
+        if val_dataloader is not None and i % 2 == 1:
             avg_eval_loss = eval(model, val_dataloader)
             print(avg_eval_loss)
             val_losses.append(avg_eval_loss)
 
     return train_losses, val_losses
+
 
 def eval(model, eval_dataloader):
     device = model.device
@@ -84,7 +88,7 @@ def eval(model, eval_dataloader):
     running_loss = 0
     with torch.no_grad():
         for batch in tqdm(eval_dataloader):
-            inputs = {k:v.to(device) for k,v in batch.items()}
+            inputs = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**inputs)
             loss = outputs.loss
 
@@ -94,11 +98,11 @@ def eval(model, eval_dataloader):
     return running_loss / len(eval_dataloader)
 
 
-
 if __name__ == "__main__":
 
     # parse yaml
-    hyperparameter_file = Path(__file__).parent / "configs" / "hyperparameters.yaml"
+    hyperparameter_file = Path(__file__).parent / \
+        "configs" / "hyperparameters.yaml"
     with open(hyperparameter_file, "r") as f:
         args = yaml.safe_load(f)
 
@@ -106,14 +110,13 @@ if __name__ == "__main__":
     print(args)
 
     torch.save(model.state_dict(), "models/e2e_prefix_tuned.pth")
-    
 
     # plt.plot(train_losses)
     # plt.xlabel("epochs")
     # plt.ylabel("average loss")
     # plt.title("training loss on medium data")
     # plt.savefig("medium_train_losses.png")
-    
+
     # plt.plot(val_losses)
     # plt.xlabel("epochs")
     # plt.ylabel("average loss")
